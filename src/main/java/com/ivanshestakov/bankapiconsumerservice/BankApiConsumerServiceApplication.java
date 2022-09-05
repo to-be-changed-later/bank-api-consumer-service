08065f69b9fc9d9d3d156a1f92d21577115b623b
VanyaShestakov <vanka1108@gmail.com>
2022-09-05 18:18:13 +0300

    initial

---
+++ src/main/java/com/ivanshestakov/bankapiconsumerservice/BankApiConsumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ivanshestakov.bankapiconsumerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiConsumerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankApiConsumerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivanshestakov/bankapiconsumerservice/BankApiConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanshestakov.bankapiconsumerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiConsumerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
